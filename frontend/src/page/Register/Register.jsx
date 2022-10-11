fca5f831a8e295e2cc883f14dd1cb93bec829c92
mrminhb <novafalcongamers@gmail.com>
2022-10-11 09:51:08 +0700

    Register feature + fields verification

--- frontend/src/page/Register/Register.jsx
+++ frontend/src/page/Register/Register.jsx
@@ -8,10 +8,10 @@ import { publicRequest } from '../../requestMethods';
 
 function Register() {
 
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [confirmPassword, setConfirmPassword] = useState();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("")
 
   const navigate = useNavigate();
@@ -19,25 +19,40 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
 
+    if(username === "" || email === "" || password === "" || confirmPassword == "") {
+      setError("Fields cannot be empty!")
+      return;
+    }
+
+    if (password.trim().length < 6) {
+      setError("Password must be at least 6 characters!")
+      return;
+    }
+
     if (password !== confirmPassword) {
       setError("Password does not match!")
       return;
     }
 
     const newUser = {
-      username,
-      email,
-      password,
+      username: username.trim(),
+      email: email.trim(),
+      password: password.trim(),
     }
 
     try {
       const res = await publicRequest.post("auth/register", newUser)
       console.log(res)
+      setError("")
+      
+      navigate("/login")
     } catch (err) {
-      console.log(err)
+      console.log(err.response.data.code)
+      const errCode = err.response.data.code
+      if (errCode == 11000) {
+        setError("Username or email already registered for an account!")
+      }
     }
-
-    console.log("Register")
   }
 
   return (
@@ -57,28 +72,28 @@ function Register() {
         <div className="wrapper">
             <h1>Create an account</h1>
             <form action="">
-                <label htmlFor="username">Username:</label>
+                <label htmlFor="username">Username: <span style={{ color: "red"}}>*</span></label>
                 <input 
                   type="text" 
                   name='username' 
                   placeholder='Enter your username...'
                   onChange={(e) => setUsername(e.target.value)}
                 />
-                <label htmlFor="email">Email:</label>
+                <label htmlFor="email">Email: <span style={{ color: "red"}}>*</span></label>
                 <input 
                   type="email" 
                   name='email' 
                   placeholder='Enter your email...'
                   onChange={(e) => setEmail(e.target.value)}
                 />
-                <label htmlFor="username">Password:</label>
+                <label htmlFor="username">Password: <span style={{ color: "red"}}>*</span></label>
                 <input 
                   type="password" 
                   name='password' 
                   placeholder='Enter your password...'
                   onChange={(e) => setPassword(e.target.value)}
                 />
-                <label htmlFor="confirm-password">Confirm password:</label>
+                <label htmlFor="confirm-password">Confirm password: <span style={{ color: "red"}}>*</span></label>
                 <input 
                   type="password" 
                   name='confirm-password' 
